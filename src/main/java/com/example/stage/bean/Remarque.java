3c58f0f7e5b8a08dc017b11c5c94abe184ecd04e
oumayma <benhniyaoumayma@gmail>
2023-04-27 16:20:23 +0100

    4

--- src/main/java/com/example/stage/bean/Remarque.java
+++ src/main/java/com/example/stage/bean/Remarque.java
@@ -4,11 +4,9 @@ package com.example.stage.bean;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.util.Date;
+import java.util.List;
 
 @Entity
 public class Remarque {
@@ -17,6 +15,16 @@ public class Remarque {
     @DateTimeFormat(pattern = "dd/mm/yyyy")
     private Date dateTime ;
     private String remarque ;
+    @OneToMany
+    private List<Projet> projets ;
+
+    public List<Projet> getProjets() {
+        return projets;
+    }
+
+    public void setProjets(List<Projet> projets) {
+        this.projets = projets;
+    }
 
     public Long getId() {
         return id;
